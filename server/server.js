24be03516ef0ee14cb02a5f3d0390f108b35edd0
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-12-07 21:22:12 -0800

    Finishes server page with basic requests

--- server/server.js
+++ server/server.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
+var parser = require('body-parser');
+var db = require('../db');
+
 
 var app = express();
 
-app.use(bodyParser.json());
+app.use(parser.json());
 
-app.post('/itemList', function() {
-  
+var newEvent = path.join(__dirname, '../db/event.js')
+var itemList = path.join(__dirname, '../db/itemlist.js')
+
+
+app.post('/eventTable', function(req, res, next) {
+  newEvent.Event(req)
+  .then(function(resp) {
+    console.log('Added Event: ', resp);
+  })
 })
-app.get('/itemList', function() {
-  
+app.get('/eventTable', function(req, res, next) {
+  newEvent.getAll()
+  .then(function(resp)) {
+    console.log('Got Events: ', resp);
+  }
 })
+
+
+app.post('/itemList', function(req, res, next) {
+  itemList.Item(req)
+  .then(function(resp) {
+    console.log('Added Item: ', resp);
+  })
+})
+app.get('/itemList', function(req, res, next) {
+  itemList.getAll()
+  .then(function(resp) {
+    console.log('Got Items: ', resp);
+  })
+})
+
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000');
