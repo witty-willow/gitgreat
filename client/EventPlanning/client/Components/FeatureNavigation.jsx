0118381432827ad2d6c24d5b387383d9b60608ee
Wells Tsai <wells.z.tsai@gmail.com>
2016-12-08 01:01:53 -0800

    Add basic button styling for bottom navigation bar

--- client/EventPlanning/client/Components/FeatureNavigation.jsx
+++ client/EventPlanning/client/Components/FeatureNavigation.jsx
@@ -1,7 +1,7 @@
 var FeatureNavigation = (props) => (
-  <div id="bottomNav">Bottom Navigation
-    <button value="whatToBringBtn" onClick={(e) => props.changeDisplay(e)}>What To Bring</button>
-    <button value="activitiesBtn" onClick={(e) => props.changeDisplay(e)}>Activities (under construction)</button>
+  <div id="bottomNav">
+    <button className="wtbBtn" value="whatToBringBtn" onClick={(e) => props.changeDisplay(e)}>What To Bring</button>
+    <button className="actBtn" value="activitiesBtn" onClick={(e) => props.changeDisplay(e)}>Activities (under construction)</button>
     <button>Another dummy feature</button>
   </div>
 );
