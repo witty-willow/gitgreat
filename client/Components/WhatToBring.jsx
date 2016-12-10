46c35704fa1080163e9c9e071a23378e862205a4
Jenn Kao <jennkao92@gmail.com>
2016-12-10 15:34:08 -0800

    add form to post to itemlist on whattobring.jsx

--- client/Components/WhatToBring.jsx
+++ client/Components/WhatToBring.jsx
@@ -1,16 +1,92 @@
 class WhatToBring extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      itemList: [{item: 'mashed potatoes', cost: '20', owner: 'Jenn'}],
+      currentItem: null,
+      currentOwner: null,
+      currentCost: null
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleItemChange = this.handleItemChange.bind(this);
+    this.handleCostChange = this.handleCostChange.bind(this);
+    this.handleOwnerChange = this.handleOwnerChange.bind(this);
+    this.fetchItems = this.fetchItems.bind(this);
+  }
+  componentDidMount() {
+    this.fetchItems();
   }
 
-  fetchTable() {
-    console.log('hit fetchtable');
+  fetchItems() {
+    var successHandler = function(data) {
+      var parsedData = JSON.parse(data);
+      this.setState({itemList: parsedData});
+    };
+    $.ajax({
+      method: 'GET',
+      url: '',
+      success: successHandler.bind(this)
+    });
+  }
+
+  handleSubmit() {
+    var item = {
+      item: this.state.currentItem,
+      cost: this.state.currentOwner,
+      owner: this.state.currentCost
+    };
+    var successHandler = function(data) {
+      this.fetchItems();
+    };
+    $.ajax({
+      method: 'POST',
+      url: '',
+      data: JSON.stringify(item),
+      success: successHandler.bind(this)
+    });
+  }
+
+  handleItemChange(event) {
+    this.setState({
+      currentItem: event.target.value
+    });
+  }
+
+  handleOwnerChange(event) {
+    this.setState({
+      currentOwner: event.target.value
+    });
+  }
+
+  handleCostChange(event) {
+    this.setState({
+      currentCost: event.target.value
+    });
   }
 
   render() {
-    this.fetchTable();
     return (
-      <div>What To Bring Page Here
+      <div>What To Bring Page
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Item: 
+            <input type='text' name='item' 
+              onChange={this.handleItemChange}
+            />
+          </label>
+          <label>
+            Cost: 
+            <input type='text' name='cost' 
+              onChange={this.handleCostChange}
+            />
+          </label>
+          <label>
+            Owner:
+          <input type='text' name='owner' 
+            onChange={this.handleOwnerChange}
+          />
+          </label>
+        </form>
         <table>
           <thead>
             <tr>
@@ -20,9 +96,13 @@ class WhatToBring extends React.Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>{}</th>
-            </tr>
+            {this.state.itemList.map( (item) => 
+              <tr>
+                <th>{item.item}</th>
+                <th>{item.cost}</th>
+                <th>{item.owner}</th>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
