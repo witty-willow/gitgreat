e1f6810fa7986e932a6dabadd3f4ed5ba99418c3
Ethan Harry <Ethan@Ethans-MacBook-Pro.local>
2016-12-13 16:44:22 -0800

    vinh is the man

--- client/Components/nav.jsx
+++ client/Components/nav.jsx
@@ -1,10 +1,12 @@
 //Child component within the HomepageApp and the CreateEventApp to toggle
 //between the two App pages.
+import {Link} from 'react-router'
+
 var Nav = ({handleCreateClick}) => (
   <div className="nav">
-  	<a href='/' className="title">Friends</a>
+    <a href='/' className="title">Friends</a>
     <a href='/'>Home </a>
-    <a onClick={handleCreateClick}>Create Event</a>
+    <Link to={'/create'} >Create Event</Link>
     <a href='#'>Log In</a>
   </div>
 );
