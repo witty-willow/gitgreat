decda64e7f948cc3933a9bb2b044828c89c30b55
Ethan Harry <Ethan@Ethans-MacBook-Pro.local>
2016-12-13 18:03:31 -0800

    stuff

--- client/Components/EventList.jsx
+++ client/Components/EventList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import EventListEntry from './EventListEntry.jsx';
+import db from '../../db/index.js';
+import $ from 'jquery'
 
 //Child component within HomepageApp
 //Will receive the eventList in an array as a prop and will create EventListEntries for each event
@@ -11,14 +13,16 @@ class EventList extends React.Component {
       completed: []
     };
   }
-  componentWillReceiveProps(nextProps) {
+  ComponentDidMount() {
     //This function will sort the events in the eventList into two categories: upcoming and completed
     //The sort logic compares the event date with the current date and checks to see if the event
     //has already passed.
-    if (nextProps.eventData) {
+    console.log('did u mount bro')
+    db.EventTable.findAll(function(data) {
+      console.log(data)
       var upcoming = [];
       var completed = [];
-      nextProps.eventData.forEach((event) => {
+      data.forEach((event) => {
         var now = new Date();
         var eventDate = new Date(event.when);
         if (eventDate >= now) {
@@ -28,7 +32,7 @@ class EventList extends React.Component {
         }
       });
       this.setState({upcoming: upcoming, completed: completed});
-    }
+    })
   }
 
   render() {
